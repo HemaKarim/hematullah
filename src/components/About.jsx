a261bd7250cb3f01609f53ac2d0be64747fd0a4b
Hematullah Karimzada <hematullahkarimzada7@gmail.com>
2024-02-28 18:06:11 -0500

    might have fixed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Tilt from "react-tilt";
+import Tilt from "react-parallax-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
@@ -8,10 +8,10 @@ import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 
 const ServiceCard = ({ index, title, description, icon }) => (
-  <Tilt className='xs:w-[250px] w-full'>
+  <Tilt className="xs:w-[250px] w-full">
     <motion.div
       variants={fadeIn("right", "spring", index * 0.5, 0.75)}
-      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+      className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
     >
       <div
         options={{
@@ -19,19 +19,19 @@ const ServiceCard = ({ index, title, description, icon }) => (
           scale: 1,
           speed: 450,
         }}
-        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+        className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
       >
         <img
           src={icon}
-          alt='web-development'
-          className='w-16 h-16 object-contain'
+          alt="web-development"
+          className="w-16 h-16 object-contain"
         />
 
-        <h3 className='text-white text-[16px] font-bold text-center'>
+        <h3 className="text-white text-[16px] font-bold text-center">
           {title}
         </h3>
 
-        <h3 className='text-white text-[16px] font-bold text-center'>
+        <h3 className="text-white text-[16px] font-bold text-center">
           {description}
         </h3>
       </div>
@@ -49,27 +49,28 @@ const About = () => {
 
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
-        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
-        I'm a highly motivated individual with a Bachelor of Engineering in 
-        Mechanical Engineering with a Mechatronics stream from 
-        Toronto Metropolitan University (formerly Ryerson University). 
-        Driven by a deep passion for both the mechanical and software engineering 
-        fields, I possess a versatile skill set that spans across various programming
-        languages, including Python, C, C#, C++, HTML5/CSS3, and JavaScript. 
-        Additionally, I am proficient in frameworks such as React, Angular, Django and Flask, 
-        alongside expertise in CAD software like AutoCAD and SolidWorks. 
-        With a strong foundation in both domains, I excel in crafting innovative 
-        solutions that seamlessly integrate hardware and software components. 
-        My analytical mindset, coupled with strong problem-solving abilities, 
-        enables me to navigate complex challenges with ease. I am committed to 
-        leveraging my education and skills to drive impactful advancements in both
-        mechanical and software engineering realms.
+        I'm a highly motivated individual with a Bachelor of Engineering in
+        Mechanical Engineering with a Mechatronics stream from Toronto
+        Metropolitan University (formerly Ryerson University). Driven by a deep
+        passion for both the mechanical and software engineering fields, I
+        possess a versatile skill set that spans across various programming
+        languages, including Python, C, C#, C++, HTML5/CSS3, and JavaScript.
+        Additionally, I am proficient in frameworks such as React, Angular,
+        Django and Flask, alongside expertise in CAD software like AutoCAD and
+        SolidWorks. With a strong foundation in both domains, I excel in
+        crafting innovative solutions that seamlessly integrate hardware and
+        software components. My analytical mindset, coupled with strong
+        problem-solving abilities, enables me to navigate complex challenges
+        with ease. I am committed to leveraging my education and skills to drive
+        impactful advancements in both mechanical and software engineering
+        realms.
       </motion.p>
 
       <h2 className={styles.sectionHeadText}>Education.</h2>
 
-      <div className='mt-20 flex flex-wrap gap-10'>
+      <div className="mt-20 flex flex-wrap gap-10">
         {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
         ))}
